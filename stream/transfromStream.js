1b7767260794c57d65b8449532b6026ddbee98cc
yian <167605868+naiyniB@users.noreply.github.com>
2026-02-10 11:02:33 +0800

    archive

--- stream/transfromStream.js
+++ stream/transfromStream.js
@@ -1,3 +1,21 @@
+const reader = new ReadableStream({
+  start(controller) {
+    this.string = "lorem ipsum dolor sit amet consectetur adipisicing elit";
+  },
+  pull(controller) {
+    const chunk = this.string.slice(0, 1);
+    this.string = this.string.slice(1);
+    controller.enqueue(chunk);
+    if (this.string.length === 0) {
+      controller.close();
+    }
+  },
+}).getReader();
 
+(async () => {
+  for await (const element of ) {
+    console.log(element);
+  }
+})();
 
 console.log("结束");
