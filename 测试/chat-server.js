543220466fd85ed3997d5a78ff86a8f7c56e80b0
yian <167605868+naiyniB@users.noreply.github.com>
2026-02-27 17:00:06 +0800

    archive

--- 测试/chat-server.js
+++ 测试/chat-server.js
@@ -2,17 +2,19 @@ const { WebSocketServer } = require("ws");
 
 // 在 8080 端口启动服务器
 const wss = new WebSocketServer({ port: 8080 });
-
+const user = [];
 wss.on("connection", (ws) => {
   console.log("新客户端连接成功！");
-
+  user.push(ws);
   // 监听客户端发来的消息
   ws.on("message", (data) => {
     // 原生收到的是 Buffer 数据，需要转成字符串
     console.log("收到消息:", data.toString());
-
-    // 回复客户端
-    ws.send("服务器已收到：" + data);
+    user.forEach((ws) => {
+      if (ws != this) {
+        ws.send("新用户已经上线，当前人数" + user.length);
+      }
+    });
   });
 
   ws.on("close", () => {
